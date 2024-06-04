fd0b31b46499a8ce252a4c233b8405bb1a451690
wins <win4709hi@gmail.com>
2024-06-05 01:17:54 +0800

    feat: 改用vue的做法 抽一格為component

---
+++ src/components/GridItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="['grid-item', { hollow: isHollow }]"></div>
+  </template>
+
+<script lang="ts" setup>
+
+defineProps<{
+    isHollow: boolean;
+  }>()
+</script>
+
+  <style scoped>
+  .grid-item {
+    background-color: #fff;
+    border: 1px solid #ddd;
+    width: 50px;
+    height: 50px;
+    box-sizing: border-box;
+  }
+
+  .hollow {
+    background-color: transparent;
+    border: none;
+  }
+  </style>
